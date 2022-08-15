8389ed51c1844a2586a1c46316e26f21a220dedd
Dipak Dushing <97525751+dipakdushing@users.noreply.github.com>
2022-08-15 14:53:32 +0530

    FE Done

---
+++ Better _Wealth-FE/src/App.js
@@ -0,0 +1,11 @@
+import Home from "./components/home/Home";
+import Login from "./components/login/Login";
+import Profile from "./components/profile/Profile";
+import Register from "./components/register/Register";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return <Profile/>;
+}
+
+export default App;
---
+++ Better _Wealth-FE/src/components/home/Home.jsx
@@ -0,0 +1,18 @@
+import Topbar from "../topbar/Topbar";
+import Sidebar from "../sidebar/Sidebar";
+import Feed from "../feed/Feed";
+import Rightbar from "../rightbar/Rightbar";
+import "./home.css"
+
+export default function Home() {
+  return (
+    <>
+      <Topbar />
+      <div className="homeContainer">
+        <Sidebar />
+        <Feed/>
+        <Rightbar/>
+      </div>
+    </>
+  );
+}
